09bd2d356e5dde27e41c0614a02b88f599952180
REIMEI <kakibo1213@gmail.com>
2021-06-06 15:42:37 +0900

    トップページ作成中 user.nameが表示されない

--- rarestudy/views/topView.py
+++ rarestudy/views/topView.py
@@ -1,10 +1,17 @@
-from django.views import generic
+from django.views.generic import ListView
 from django.contrib.auth import (
      get_user_model, logout as auth_logout,
 )
+from rarestudy.models.article import Article
 
 User = get_user_model()
 
 
-class Top(generic.TemplateView):
+class Top(ListView):
     template_name = 'top/top.html'
+    model = Article
+    paginate_by = 5
+    context_object_name = 'Articles'
+
+    def get_queryset(self):
+        return Article.objects.order_by('-created_at')[:100]
