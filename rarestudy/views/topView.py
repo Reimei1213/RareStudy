f1ee9ac9be2e134000b9a57fe450e141f1983753
REIMEI <34594195+Reimei1213@users.noreply.github.com>
2021-06-09 01:36:30 +0900

    Revert "Revert "15_トップページ実装""

--- rarestudy/views/topView.py
+++ rarestudy/views/topView.py
@@ -1,10 +1,17 @@
-from django.views import generic
+from django.views.generic import ListView
 from django.contrib.auth import (
      get_user_model, logout as auth_logout,
 )
+from rarestudy.models.article import Article
 
 User = get_user_model()
 
 
-class Top(generic.TemplateView):
+class Top(ListView):
     template_name = 'top/top.html'
+    model = Article
+    paginate_by = 10
+    context_object_name = 'Articles'
+
+    def get_queryset(self):
+        return Article.objects.filter(valid=True).order_by('-created_at')[:100]
