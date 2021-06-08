51839f22bcd3e84d2459c3ce5eea7a0e25f57f13
REIMEI <kakibo1213@gmail.com>
2021-06-09 00:44:25 +0900

    top page実装

--- rarestudy/views/topView.py
+++ rarestudy/views/topView.py
@@ -10,8 +10,8 @@ User = get_user_model()
 class Top(ListView):
     template_name = 'top/top.html'
     model = Article
-    paginate_by = 5
+    paginate_by = 10
     context_object_name = 'Articles'
 
     def get_queryset(self):
-        return Article.objects.order_by('-created_at')[:100]
+        return Article.objects.filter(valid=True).order_by('-created_at')[:100]
