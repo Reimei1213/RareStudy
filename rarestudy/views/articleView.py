c8032bef5fe584d0bd1475f81861dd6c9fd08547
森本真也 <shinkinoko1801@gmail.com>
2021-06-20 18:58:29 +0900

    指摘箇所修正

--- rarestudy/views/articleView.py
+++ rarestudy/views/articleView.py
@@ -2,11 +2,8 @@ from django.views.generic import DetailView, CreateView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.contrib.auth import (get_user_model, logout as auth_logout,)
-from rarestudy.models import article
 from rarestudy.models.article import Article
 from rarestudy.forms import AddArticleForm
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
 User = get_user_model()
 
@@ -33,14 +30,5 @@ class Edit(UpdateView):
     model = Article
     form_class = AddArticleForm
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
     def get_success_url(self):
         return reverse('rarestudy:article/detail', kwargs={'pk':self.object.id})
-
-@login_required
-def detailfunction(request, pk):
-    article = Article.objects.get(pk=pk)
-    return render(request, 'article/detail.html',{"article":article})
