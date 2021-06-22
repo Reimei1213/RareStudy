30f0fbc963c2a75a3a3d65a36dc2e5f86fe383d3
森本真也 <shinkinoko1801@gmail.com>
2021-06-22 09:08:48 +0900

    [WIP]32_コメント編集ページ

--- rarestudy/views/commentView.py
+++ rarestudy/views/commentView.py
@@ -1,9 +1,11 @@
-from django.views.generic import CreateView
+from django.urls.base import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from rarestudy.models.article import Article
 from rarestudy.models.comment import Comment
 from rarestudy.forms import AddCommentForm
+from django.shortcuts import redirect
 
 class Add(LoginRequiredMixin, CreateView):
     template_name = 'comment/add.html'
@@ -17,3 +19,12 @@ class Add(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('rarestudy:article/detail', kwargs={'pk':self.kwargs['pk']})
+
+
+class Edit(UpdateView):
+    template_name = 'comment/edit.html'
+    model = Comment
+    form_class = AddCommentForm
+
+    def get_success_url(self):
+     return reverse('rarestudy:article/detail', kwargs={'pk':self.kwargs['pk']})
